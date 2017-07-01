2b772791668feb69c0ab27520583728cd386bae6
ytsai43 <ytsai43@asu.edu>
2017-07-01 12:33:19 -0700

    Update partition scheme.py

--- partition scheme.py
+++ partition scheme.py
@@ -1,17 +1,27 @@
 class Subregion:
-    def __init__(self,x1,x2):
+    def __init__(self,x1,x2,dx):
         self.vertx1 = x1
         self.vertx2 = x2
+        self.dx = dx
     def cut(self):
         self.subPoints1 = []
         self.subPoints2 = []
-        for a in range(2):
-            self.subPoints1.append((self.vertx1[0][a]+self.vertx1[1][a])/2)   # middle point we need in an interval    
-            self.subPoints2.append((self.vertx2[0][a]+self.vertx2[1][a])/2)   # middle point we need in an interval
-        self.vertx1.append(self.subPoints1)
-        self.vertx2.append(self.subPoints2)
-        self.vertx1.sort(key = lambda x: (x[-1]))
-        self.vertx2.sort(key = lambda x: (x[-1]))
+        for i in range(self.dx-1):
+            for a in range(2):
+                if a != 0:
+                    self.subPoints1.append(round((self.vertx1[0][a]+self.vertx1[1][a])*((i+1)/self.dx),2))   # middle points we need in an interval    
+                    self.subPoints2.append(round((self.vertx2[0][a]+self.vertx2[1][a])*((i+1)/self.dx),2))   # middle points we need in an interval
+                else: 
+                    self.subPoints1.append(round((self.vertx1[0][a]+self.vertx1[1][a])/2,2))   # middle points we need in an interval    
+                    self.subPoints2.append(round((self.vertx2[0][a]+self.vertx2[1][a])/2,2))   # middle points we need in an interval
+            self.vertx1.append(self.subPoints1)
+            self.vertx2.append(self.subPoints2)
+            self.subPoints1 = []
+            self.subPoints2 = []
+        self.vertx1.sort(key = lambda x: (x[-1]))  # rank y by specific x1 value
+        self.vertx2.sort(key = lambda x: (x[-1]))  # rank y by specific x2 value
+        print(self.vertx1)
+        print(self.vertx2)
     def pickPoint(self,M):
         self.seg = []
         j = 0
@@ -31,12 +41,6 @@ class Subregion:
 
             
 feasible=[[[[0, 0], [0, 2]], [[2, 0], [2, 2]]]]
-for j in range(3): 
-    subset=[]
-    for i in range(len(feasible)):
-        first = Subregion(feasible[i][0],feasible[i][1])
-        first.cut()
-        first.pickPoint(2)  
-        subset= subset + first.pickPoint(2)
-    print(subset)
-    feasible=subset[:]
+first = Subregion(feasible[0][0],feasible[0][1],2)
+first.cut()
+first.pickPoint(2)
